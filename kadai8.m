1018ae1393710126a44ee36af84cc5bbbba2037b
misaki <idolmaster.hibiki1010@gmail.com>
2016-07-11 20:41:52 +0900

    Update kadai8.m

--- kadai8.m
+++ kadai8.m
@@ -4,7 +4,7 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ． 
 % 例
 
-ORG = imread('Lenna.jpg'); % 画像の読み込み
+ORG = imread('https://kamigame.jp/%E3%82%B0%E3%83%A9%E3%83%96%E3%83%AB/%E3%82%AD%E3%83%A3%E3%83%A9%E3%82%AF%E3%82%BF%E3%83%BC/SSR/img/%E3%82%BD%E3%82%B7%E3%82%A8BU.png'); % 画像の読み込み
 ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 pause;
