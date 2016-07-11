8dca21b16b9806d16163355f3d57d774f83a5c43
misaki <idolmaster.hibiki1010@gmail.com>
2016-07-11 20:40:32 +0900

    Update kadai6.m

--- kadai6.m
+++ kadai6.m
@@ -4,7 +4,7 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 clear; % 変数のオールクリア
-ORG=imread('Lenna.png'); % 原画像の入力
+ORG=imread('http://jlab-cache.portfolio24.xyz/jlab-ssd/anime/s/ssd143956514036532.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG);
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
 pause; % 一時停止
